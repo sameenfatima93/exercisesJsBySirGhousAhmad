6ab8b70099179f2927b34bc94c6b57a5051ad5eb
SameenFatima <sameen.fatima993@gmail.com>
2025-06-23 08:36:40 +0500

    jsExercisesChepter-1

---
+++ Exersize-1/alert.js
@@ -0,0 +1,17 @@
+// <==========**Alert**========> //
+
+// task_1
+// alert("hello world")
+//-----------------------------------------------
+//task_2
+// alert("Error! please Enter a valid paasword")
+//-----------------------------------------------
+//task_3
+// alert("welcome to js land.. \n Happy coding") // use '\n' fo line break in javascript
+//--------------------------------------------
+//task_4
+// alert("welcome to js land")
+// alert("happy coding  ")
+//-----------------------------------------------
+//task_5
+// console.log("hello i can run js trough my web browser's console");
