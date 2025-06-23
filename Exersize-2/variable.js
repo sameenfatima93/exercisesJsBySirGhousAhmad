9de51fb7a8ef3186765b84ddfc4e9e18caef459c
SameenFatima <sameen.fatima993@gmail.com>
2025-06-23 09:31:32 +0500

    chapter_2-taskDone

---
+++ Exersize-2/variable.js
@@ -0,0 +1,58 @@
+//task_1
+// var usrName = "sameen"
+//--------------------------------------------------------
+//task_2
+// var myName = "fatima"
+// var fullname = "sameen fatima"
+//--------------------------------------------------------
+//task_3
+// var message = "Hello wolrd"
+// alert(message);
+//--------------------------------------------------------
+//task_4
+// var studentName = "Ameer"
+// var age = "23 years old"
+// var education = "certified mobile application developer"
+// var bioData = studentName + "\n" + age + "\n" + education;
+// alert(bioData) // 3ree mdata in one alert
+// alert(studentName) // every data and deferent alert
+// alert(age)
+// alert(education)
+//---------------------------------------------------------
+// task_5
+// var dish = "pizza"
+// alert(dish)
+// dish ="pizz"
+// alert(dish)
+// dish ="piz"
+// alert(dish)
+// dish ="pi"
+// alert(dish)
+// dish = "p"
+// alert(dish)
+// lets try onother way
+// var msg =" pizza \n pizz \n piz \n pi \n p"
+// alert(msg) // perfect
+//--------------------------------------------------------
+//task_6
+//  var email = "example@gmail.com"
+//  alert("my email add ress is " + email)
+//-------------------------------------------------------
+// task_7
+// var book = "Asmarter way to learn JavaScript"
+// alert("i am trying to learn form the book" + " \n " + book)
+//------------------------------------------------------
+//task_8
+// document.write("<h1>yah! i can write HTML content through JavaScript</h1>");
+// java script is a case sensitive language document alert console ets must in have small later
+//-------------------------------------------
+// task_9
+// var endMsg = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(endMsg);
+// document.write("<h1>yah! i can write HTML content through JavaScript</h1> + \n + “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+//                  <=======<*><*>---- chepter_2 all task complete -----<*><*>============>
+
+
+
+
